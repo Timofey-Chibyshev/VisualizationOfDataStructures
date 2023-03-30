83231c0ee8de0d6042c308f426e611e60ac2a63e
Timofey-Chibyshev <90447625+Timofey-Chibyshev@users.noreply.github.com>
2023-03-30 21:26:44 +0300

    work version

---
+++ develop/demo1/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module application {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
+
+    opens application to javafx.fxml;
+    exports application;
+}
